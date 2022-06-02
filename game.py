082cbeb38678ee9faa6f3a0e3bae8d88462873cb
JessaTehCrow <jellekok.3.4@gmail.com>
2022-06-02 10:12:52 +0200

    Start of creature interactions

--- game.py
+++ game.py
@@ -1,14 +1,19 @@
-from network import Network, random_network
+import numpy as np
 import pygame
 
+from network import Network, random_network
+from typing import List
+
+
 class Creature():
-    def __init__(self, network:Network, thrust_power:float, map_size:list):
+    def __init__(self, network:Network, thrust_power:float, map_size:list, gravity:float):
         self.nn = network
 
-        self.pos = [0,0]
         self.size = map_size
+        self.pos = [self.size[0]/2, self.size[1]/2]
 
         self.thrust = thrust_power
+        self.gravity = gravity
         self.forces = [0, 0]
         self.velocity = [0, 0]
 
@@ -22,12 +27,20 @@ class Creature():
 
     # Predict solution from senario input
     def predict(self, inputs:list):
+
         return self.nn.predict(inputs)
     
 
     # Next step
     def next(self):
-        self.forces = self.predict()
+        middle_offset = (self.size[1]/2 - self.pos[1])*2/self.size[1]
+        predictions = self.predict([middle_offset])
+        thrust = predictions[0] > predictions[1]
+        self.forces = [0,thrust*self.thrust - self.gravity]
+
+        self.velocity = [(x/60)+y for x,y in zip(self.forces,self.velocity)]
+        print(self.velocity)
+        
 
 
 class Game():
@@ -35,17 +48,18 @@ class Game():
         self.width = width
         self.height = height
         self.gravity = gravity
-        self.ais = []
+        self.ais:List[Creature] = []
     
     
     # Add network to game data
-    def register_network(self, ai:Network):
-        self.ais.append( Creature(ai), [self.width,self.height] )
+    def register_network(self, ai:Network, thrust_power:float):
+        self.ais.append( Creature(ai, thrust_power, [self.width,self.height], self.gravity) )
     
 
     # Next step for all registered AIs
     def next(self):
-        pass
+        for ai in self.ais:
+            ai.next()
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from network import random_network, mate, Network
+from game import Game
 """
 This file is mainly used for personal debugging whilst creating this repo.
 This file might get removed or used for other purposes later down the line.
@@ -8,16 +9,13 @@ This file might get removed or used for other purposes later down the line.
 inputs        = 1
 hidden_layers = 1
 hidden        = 2
-outputs       = 1
+outputs       = 2
 
 # Parent neural networks
-network1 = Network(1,[[[1,1]],[[1],[1]],[None]], [[None],[1,1], [1]])
-network2 = Network(2,[[[2,2]],[[2],[2]],[None]], [[None],[2,2], [2]])
+network1 = random_network(inputs, hidden_layers, hidden, outputs)
 
-
-# Debug mating
-child = mate(network1,network2, 0.03)
+game = Game(150, 100, 3.6)
+game.register_network(network1, 5)
 
 # Get new values
-print(child.biases)
-print(child.weights)
+game.next()
